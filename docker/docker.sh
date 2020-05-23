f49424f1bb575e10b57d3da071079430cb8c5f37
yao <yaoshuyin@gmail.com>
2020-05-24 01:19:11 +0800

    ...

--- docker/docker.sh
+++ docker/docker.sh
@@ -3,6 +3,6 @@
 
 for c in $(/usr/bin/docker ps | awk 'NR>1 {print $1}')
 do
-   /usr/bin/docker container exec $c /usr/local/bin/init.sh
+   /usr/bin/docker container exec $c /data/init.sh
    sleep 2
 done
